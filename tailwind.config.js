8000c48ed7c2307954ef25845bf7c90a11f067a7
Elbshat <mahmoudelbshat7@gmail.com>
2022-03-31 03:01:45 +0200

    deploy

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "main-black": "#2c2a27",
+        "dark-gold": "#54514b",
+        "section-bg": "#EDECE7",
+      },
+    },
+  },
+  plugins: [],
+};
